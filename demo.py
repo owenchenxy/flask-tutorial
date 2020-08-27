f4dd39452d1d7289a6284783f66c89f7bf7fcb00
cxy <owenchen@yunify.com>
2020-08-27 17:19:32 +0800

    form

--- demo.py
+++ demo.py
@@ -5,18 +5,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<color>')
-def color(color):
-    default_style = 'style/default_style.html'
-    if color != 'red' and color != 'green':
-        style_html = default_style
-    else:
-        style_html = 'style/{0}_style.html'.format(color)
-    parameter = {
-        'color': color,
-        'style_html': style_html,
-    }
-    return render_template('color.html', **parameter)
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__=='__main__':
 	app.run(debug=True)
